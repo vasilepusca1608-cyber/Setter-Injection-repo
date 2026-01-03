6e6ab94e19e22a48f1253671b433dec07dee6447
Pusca Vasile <vasea-pusca@mail.ru>
2026-01-03 11:32:01 +0200

    Add new annotation.Primary

--- src/main/java/pixel/academy/setter_injection/common/FrenchChef.java
+++ src/main/java/pixel/academy/setter_injection/common/FrenchChef.java
@@ -1,8 +1,10 @@
 package pixel.academy.setter_injection.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class FrenchChef implements Chef {
 
     @Override
